4a115108851da28f2e7a29869f0a182f95538fe0
BattleCh1cken <BattleCh1cken@larkov.de>
2023-07-27 11:27:47 -0400

    Added debug info for Void16

--- flake.nix
+++ flake.nix
@@ -5,23 +5,26 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-
-
-        devShell = with pkgs; mkShell {
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShell = with pkgs;
+        mkShell {
           shellHook = ''
 
           '';
           buildInputs = with pkgs; [
             qmk
             clang-tools
+            bear
+            hid-listen
             avrdude
           ];
         };
-      });
+    });
 }
--- keyboards/void16/rules.mk
+++ keyboards/void16/rules.mk
@@ -1,3 +1,5 @@
 # This file intentionally left blank
-#
+
 BOOTLOADER = caterina
+CONSOLE_ENABLE = yes
+DEBUG_ENABLE = yes
